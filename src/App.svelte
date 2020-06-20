a7e55caa79718d6855163638f0be5e8379d6c62b
moru <e1028092@student.tuwien.ac.at>
2020-06-20 18:56:01 +0200

    Main description.

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script>
   import 'bulma/css/bulma.css'
   //   import { Button } from 'svelma'
-  import Button from 'svelma/src/components/Button.svelte'
+  //   import Button from 'svelma/src/components/Button.svelte'
   import Gallery from './Gallery.svelte'
+  import MainDescription from './MainDescription.svelte'
   import Navbar from './Navbar.svelte'
-  export let name
+  //   export let name
 </script>
 
 <style>
@@ -13,13 +14,6 @@
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
   }
 
   @media (min-width: 640px) {
@@ -35,14 +29,7 @@
 
   <section id="product" class="columns section">
     <Gallery class="column is-one-quarter" />
+    <MainDescription class="column is-half" />
+
   </section>
-
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the
-    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-    to learn how to build Svelte apps.
-  </p>
 </main>
-
-<Button type="is-primary">I'm a Button!</Button>
---
+++ src/MainDescription.svelte
@@ -0,0 +1,71 @@
+<script>
+  let clazz = ''
+  export { clazz as class }
+</script>
+
+<style>
+  .rating {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+  }
+  .stars {
+    flex-grow: 1;
+
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+  }
+
+  .comments {
+    flex-grow: 0;
+  }
+
+  .nr-of-ratings {
+    /* margin-lef */
+  }
+</style>
+
+<div class="column is-half content has-text-left {clazz}">
+  <h1>Connection Wristband</h1>
+
+  <div class="rating">
+    <div class="stars">
+      <img src="../star-full.svg" alt="full star" />
+      <img src="../star-full.svg" class="ml-1" alt="full star" />
+      <img src="../star-full.svg" class="ml-1" alt="full star" />
+      <img src="../star-full.svg" class="ml-1" alt="full star" />
+      <img src="../star-empty.svg" class="ml-1" alt="empty star" />
+      <a href="./" class="nr-of-ratings ml-2">(51)</a>
+    </div>
+    <a href="./" class="comments">51 comments</a>
+  </div>
+
+  <p>
+    Aliquam sed tincidunt rutrum, aliquam vel sem. Proin vehicula tempor enim.
+    Ut dignissim at justo at vulputate. Nunc quis hendrerit elit, id ultrices
+    libero. Morbi lobortis in justo ut dapibus. Sed ultricies, erat in mollis
+    semper, enim nisl interdum erat, ut euismod massa ante in magna.
+  </p>
+
+  <ul>
+    <li>
+      Integer varius lectus vel sagittis auctor. Curabitur id tempor arcu.
+    </li>
+    <li>Phasellus sed consequat dolor, eget pulvinar nisl.</li>
+    <li>
+      Suspendisse vel dui tempor diam blandit scelerisque a ut sem. Praesent at
+      sollicitudin felis. Aenean tellus nibh, faucibus ac lectus quis, tincidunt
+      consectetur sem.
+    </li>
+    <li>
+      Phasellus mi nunc, aliquam sed tincidunt rutrum, aliquam vel sem. Proin
+      vehicula tempor enim. Ut dignissim at justo at vulputate. Nunc quis
+      hendrerit elit, id ultrices libero. Morbi lobortis in justo ut dapibus.
+      Sed ultricies, erat in mollis semper, enim nisl interdum erat, ut euismod
+      massa ante in magna.
+    </li>
+  </ul>
+  <p />
+
+</div>
