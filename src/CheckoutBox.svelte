8fc80300584fd8870a4edce0625982f41854ff3d
moru <e1028092@student.tuwien.ac.at>
2020-06-21 18:00:09 +0200

    Auto-generated delivery-date.

--- src/CheckoutBox.svelte
+++ src/CheckoutBox.svelte
@@ -12,15 +12,36 @@
       currency: 'EUR',
     }).format(price),
   )
+
+  let deliveryDate = new Date(Date.now())
+  deliveryDate.setDate(deliveryDate.getDate() + 17)
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
+  const deliveryDateStr = deliveryDate.toLocaleDateString('en-US', options)
+
+  //   const
 </script>
 
 <div class={clazz}>
-  <p>{priceStr}</p>
-  <!-- <p> Delivery until 20-25 August 2020
-</p> -->
-  <p>Currently not deliverable.</p>
-  <Button type="is-primary">
-    <Icon pack="fas" icon="cart-plus" />
-    <span>Add to Cart</span>
-  </Button>
+  <div class="box has-text-left">
+    <p class="is-size-4">{priceStr}</p>
+    <p>+€5.00 shipping</p>
+    <div class="delivery mt-4">
+      <h2 class="is-size-7">Delivery:</h2>
+      <!-- <p>Currently not deliverable.</p> -->
+      <p>until {deliveryDateStr}</p>
+    </div>
+    <!-- <div class="delivery mt-5">
+      <h2 class="is-size-7">Quantity:</h2>
+      DROPDOWN HERE
+    </div> -->
+    <Button type="is-primary mt-5">
+      <Icon pack="fas" icon="cart-plus" />
+      <span>Add to Cart</span>
+    </Button>
+  </div>
 </div>
