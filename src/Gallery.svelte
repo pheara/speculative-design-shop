3dab05b5ace1b7fa23c4aae261a8e73a3cd12e1c
moru <e1028092@student.tuwien.ac.at>
2020-06-21 15:26:36 +0200

    Mixing flex and grid for gallery that behaves as intended.

--- src/Gallery.svelte
+++ src/Gallery.svelte
@@ -31,45 +31,22 @@
 </script>
 
 <style>
-  /* .gallery {
-    display: grid;
-    grid-template-columns: [col1-start] 1fr 1fr 1fr 1fr [lastcol-end];
-    grid-template-rows: 4fr 64px;
-
-    grid-column-gap: 0.25rem;
-    grid-row-gap: 0.25rem;
-
-    justify-items: center;
-    align-items: center;
-  }
-  .largeImg {
-    grid-column-start: col1-start;
-    grid-column-end: lastcol-end;
-    grid-row-start: 1;
-    grid-row-end: 1;
-  } */
-
   .gallery {
     display: flex;
     flex-direction: column;
   }
   .thumbs {
-    flex-grow: 1;
+    margin-top: 0.25rem;
 
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    /* justify-content: space-between; */
-    justify-content: flex-start;
-    align-items: flex-start;
-    align-content: flex-start;
+    display: grid;
+    grid-template-columns: [col1-start] 1fr 1fr 1fr 1fr [lastcol-end];
+    grid-template-rows: 1fr;
+    grid-column-gap: 0.25rem;
+    grid-row-gap: 0.25rem;
   }
 
   .thumb {
     border: 1px solid grey;
-
-    margin-top: 0.25rem;
-    margin-right: 0.25rem;
   }
 </style>
 
@@ -80,23 +57,9 @@
     alt={productPictures[0].alt} />
   <div class="thumbs">
     {#each productPictures as img}
-      <figure class="thumb image is-64x64">
+      <figure class="thumb image">
         <img src={img.src} alt={img.alt} />
       </figure>
     {/each}
   </div>
 </div>
-
-<!-- 
-<div class="gallery {clazz}">
-  <img
-    class="largeImg"
-    src={productPictures[0].src}
-    alt={productPictures[0].alt} />
-  {#each productPictures as img}
-    <figure class="thumb image is-64x64">
-      <img src={img.src} alt={img.alt} />
-    </figure>
-  {/each}
-</div> 
--->
