73b268ca82d0e770fa257b4c8eeeb866671485ab
moru <e1028092@student.tuwien.ac.at>
2020-06-21 15:20:37 +0200

    Fix gallery sizing
    Using flexbox.

--- src/Gallery.svelte
+++ src/Gallery.svelte
@@ -23,11 +23,15 @@
       alt: 'TODO the product (5)',
       src: 'https://via.placeholder.com/500x500.png?text=image+of+product+5',
     },
+    {
+      alt: 'TODO the product (6)',
+      src: 'https://via.placeholder.com/500x500.png?text=image+of+product+6',
+    },
   ]
 </script>
 
 <style>
-  .gallery {
+  /* .gallery {
     display: grid;
     grid-template-columns: [col1-start] 1fr 1fr 1fr 1fr [lastcol-end];
     grid-template-rows: 4fr 64px;
@@ -43,13 +47,47 @@
     grid-column-end: lastcol-end;
     grid-row-start: 1;
     grid-row-end: 1;
+  } */
+
+  .gallery {
+    display: flex;
+    flex-direction: column;
+  }
+  .thumbs {
+    flex-grow: 1;
+
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    /* justify-content: space-between; */
+    justify-content: flex-start;
+    align-items: flex-start;
+    align-content: flex-start;
   }
 
   .thumb {
     border: 1px solid grey;
+
+    margin-top: 0.25rem;
+    margin-right: 0.25rem;
   }
 </style>
 
+<div class="gallery {clazz}">
+  <img
+    class="largeImg"
+    src={productPictures[0].src}
+    alt={productPictures[0].alt} />
+  <div class="thumbs">
+    {#each productPictures as img}
+      <figure class="thumb image is-64x64">
+        <img src={img.src} alt={img.alt} />
+      </figure>
+    {/each}
+  </div>
+</div>
+
+<!-- 
 <div class="gallery {clazz}">
   <img
     class="largeImg"
@@ -60,4 +98,5 @@
       <img src={img.src} alt={img.alt} />
     </figure>
   {/each}
-</div>
+</div> 
+-->
