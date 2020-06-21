d483e674652204f22dd4519c82159299892db259
moru <e1028092@student.tuwien.ac.at>
2020-06-21 19:31:03 +0200

    filter util function.

---
+++ src/utils.js
@@ -0,0 +1,22 @@
+/**
+ * Shuffles array in place. ES6 version
+ * @param {Array} a items An array containing the items.
+ */
+export function shuffleArray(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
+export function filterObject(obj, f) {
+  let res = {};
+  Object.keys(obj).forEach((k) => {
+    const v = obj[k];
+    if (f(v)) {
+      res[k] = v;
+    }
+  });
+  return res;
+}
